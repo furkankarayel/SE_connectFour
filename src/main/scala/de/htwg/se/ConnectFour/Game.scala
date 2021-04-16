2fff93f407ba04a2ec0f2b932f06fd611c729199
furkankarayel <furkarayel@hotmail.com>
2021-04-16 10:54:25 +0200

    Project Structure

---
+++ src/main/scala/de/htwg/se/ConnectFour/Game.scala
@@ -0,0 +1,10 @@
+package de.htwg.se.ConnectFour
+import de.htwg.se.ConnectFour.model.Player
+
+object Game {
+
+  def main(args: Array[String]): Unit = {
+    Player("Clemens")
+    println("Hello!"+ Player)
+  }
+}
---
+++ src/test/scala/de/htwg/se/ConnectFour/Connect4.scala
@@ -0,0 +1,8 @@
+package de.htwg.se.ConnectFour
+
+object Connect4 {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello!")
+  }
+}
