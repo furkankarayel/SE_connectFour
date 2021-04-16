4c8535a3ab4e9f3ae766d689a53561de6ec291ca
furkankarayel <furkarayel@hotmail.com>
2021-04-16 11:29:36 +0200

    Revert "Project Structure"
    This reverts commit 2fff93f4

--- build.sbt
+++
@@ -1,5 +0,0 @@
-name := "ConnectFour"
-
-version := "0.1"
-
-scalaVersion := "2.13.5"
--- src/test/scala/de/htwg/se/ConnectFour/Connect4.scala
+++
@@ -1,8 +0,0 @@
-package de.htwg.se.ConnectFour
-
-object Connect4 {
-
-  def main(args: Array[String]): Unit = {
-    println("Hello!")
-  }
-}
