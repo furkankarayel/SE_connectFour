c4eb2944b8d42c87ba0e00c4d667aa9bbbde2435
furkankarayel <furkarayel@hotmail.com>
2021-04-21 23:05:53 +0200

    Player fix selecting color by number

--- src/main/scala/de/htwg/se/ConnectFour/model/Player.scala
+++ src/main/scala/de/htwg/se/ConnectFour/model/Player.scala
@@ -1,7 +1,12 @@
 package de.htwg.se.ConnectFour.model
 
-case class Player(playerName: String, playerColor: String) {
+case class Player(playerName: String, playerNumber: Int) {
   val name: String = playerName
-  val color: String = playerColor
-  override def toString:String = "Der Spieler " + name + " hat die Farbe "+ color
+  val number: Int = playerNumber
+  val color: String = number match {
+    case 1 => "red"
+    case 2 => "yellow"
+  }
+  override def toString:String = "Der Spieler " + name + " mit der Nummer "+
+                                  number + " hat die Farbe " + color
 }
--- src/test/scala/de/htwg/se/ConnectFour/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/ConnectFour/model/PlayerSpec.scala
@@ -6,16 +6,20 @@ import org.scalatest.matchers.should.Matchers
 class PlayerSpec extends AnyWordSpec with Matchers {
   "A Player" when {
     "new" should {
-      val player = Player("Your Name", "Your Color")
+      val player = Player("Your Name", 1)
       "have a name" in {
         player.name should be ("Your Name")
       }
+      "have a number" in{
+        player.number should be (1)
+      }
       "have a color" in {
-        player.color should be ("Your Color")
+        player.color should be ("red")
       }
       "have a nice String representation" in {
         player.toString should be
-        "Der Spieler " + player.name + " hat die Farbe "+ player.name
+        "Der Spieler " + player.name + " mit der Nummer "+
+          player.number +" hat die Farbe "+ player.color
       }
     }
   }
