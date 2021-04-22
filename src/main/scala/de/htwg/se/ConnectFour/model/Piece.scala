13542afc61a2fd622610a0fb4376c75753be5354
furkankarayel <furkarayel@hotmail.com>
2021-04-22 14:50:28 +0200

    Piece with test added

--- src/main/scala/de/htwg/se/ConnectFour/model/Piece.scala
+++ src/main/scala/de/htwg/se/ConnectFour/model/Piece.scala
@@ -1,5 +1,8 @@
 package de.htwg.se.ConnectFour.model
 
-case class Piece() {
-
+case class Piece(player: Player) {
+  override def toString: String = player.playerNumber match {
+    case 1 => "red"
+    case 2 => "yellow"
+  }
 }
--- src/test/scala/de/htwg/se/ConnectFour/model/PieceSpec.scala
+++ src/test/scala/de/htwg/se/ConnectFour/model/PieceSpec.scala
@@ -1,5 +1,17 @@
 package de.htwg.se.ConnectFour.model
 
-class PieceSpec {
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
+class PieceSpec extends AnyWordSpec with Matchers {
+  "A Piece" when {
+    "new" should {
+      val player = Player("Your Name", 1)
+      val piece = Piece(player)
+      "have a color" in {
+        piece.toString should be ("red")
+      }
+    }
+
+  }
 }
