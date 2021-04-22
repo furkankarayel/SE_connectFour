bc5d286dc31ffda9fe872cfdfb55c919068efa9e
furkankarayel <furkarayel@hotmail.com>
2021-04-22 14:49:43 +0200

    CellSpec fix after parameter change to String

--- src/test/scala/de/htwg/se/ConnectFour/model/CellSpec.scala
+++ src/test/scala/de/htwg/se/ConnectFour/model/CellSpec.scala
@@ -7,9 +7,9 @@ class CellSpec extends AnyWordSpec with Matchers {
 
   "A Cell" when {
     "not set to any value " should {
-      val emptyCell = Cell("0")
-      "have value 0" in {
-        emptyCell.value should be(0)
+      val emptyCell = Cell("")
+      "have no value" in {
+        emptyCell.value should be("")
       }
       "not be set" in {
         emptyCell.isSet should be(false)
@@ -18,7 +18,7 @@ class CellSpec extends AnyWordSpec with Matchers {
     "set to a specific value" should {
       val nonEmptyCell = Cell("5")
       "return that value" in {
-        nonEmptyCell.value should be(5)
+        nonEmptyCell.value should be("5")
       }
       "be set" in {
         nonEmptyCell.isSet should be(true)
