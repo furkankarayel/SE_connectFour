2ccc3a422d8a6c9ebad9597b4e349ef00cf090c6
furkankarayel <furkarayel@hotmail.com>
2021-04-28 19:16:09 +0200

    Grid fix to 6 rows 7 columns

--- src/main/scala/de/htwg/se/ConnectFour/model/Grid.scala
+++ src/main/scala/de/htwg/se/ConnectFour/model/Grid.scala
@@ -2,7 +2,7 @@ package de.htwg.se.ConnectFour.model
 
 
 case class Grid(rows: Vector[Vector[Cell]]) {
-  def this() = this(Vector.tabulate(5, 6) { (row, col) => Cell(None) })
+  def this() = this(Vector.tabulate(6, 7) { (row, col) => Cell(None) })
   def cell(row: Int, col: Int): Cell = rows(row)(col)
   def replaceCell(row: Int, col: Int, cell: Cell): Grid = copy(rows.updated(row, rows(row).updated(col, cell)))
 }
