9601d6a7280f99173b73f7327f2a419b31aca192
victor <victor.g1@web.de>
2021-04-28 22:59:09 +0200

    tui update tui update

--- src/main/scala/de/htwg/se/ConnectFour/aUI/TUI.scala
+++ src/main/scala/de/htwg/se/ConnectFour/aUI/TUI.scala
@@ -1,48 +1,39 @@
 package de.htwg.se.ConnectFour.aUI
 
 import de.htwg.se.ConnectFour._
+import de.htwg.se.ConnectFour.controller.Controller
 import de.htwg.se.ConnectFour.model.Piece
 
 import scala.io.StdIn.readLine
 
-object TUI {
-  val game = new Game()
-
-  def main(args: Array[String]): Unit = {
-    var input: String = ""
-    gamestart()
-    do {
-      input = readLine()
-      processInputLine(input)
-    } while (input != "q")
-  }
+class TUI(controller: Controller) {
 
   def processInputLine(input: String):Unit = {
     input match {
       //case "n"=> Board().grid
-      case "p" => print(game.board)
+      case "p" => controller.gridPrint()
       case "q" => println("Tschüss")
-      case _ => {
+      case _ =>
         input.toList.filter(c => c != " ").map(c => c.toString.toInt) match {
-          case col :: Nil => {
-            game.player = if(game.move % 2 == 0)  game.player1 else game.player2
-            game.board.drop(col, Piece(game.player))
-            game.move+=1
-            print(game.board)
-          }
-          case _ => print(game.board)
+          case col :: Nil =>
+            Game.player = if(Game.move % 2 == 0)  Game.player1 else Game.player2
+            controller.dropPiece(col, Piece(Game.player))
+            Game.move+=1
+            print(controller.gridPrint)
+          case _ => print(controller.gridPrint)
         }
-      }
     }
   }
 
   def gamestart(): Unit ={
-    println("Welcome to Connect Four")
-
+    println("################################")
+    println("####Welcome to Connect Four#####")
+    println("################################")
+    println("")
     println("You can start playing with your partner by typing a column number 0-6")
     println("After each move it will be the turn of the other player")
     println("Good luck and have fun!")
-
+    println("")
     println("To print the latest board press 'p'")
     println("_______________________________")
     println("If you want to quit the game press 'q'")
