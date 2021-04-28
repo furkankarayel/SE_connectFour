bfe821f2acb5ed5b834e05376f5cc2116609f02b
furkankarayel <furkarayel@hotmail.com>
2021-04-28 21:52:34 +0200

    moving board to grid

--- src/main/scala/de/htwg/se/ConnectFour/model/Grid.scala
+++ src/main/scala/de/htwg/se/ConnectFour/model/Grid.scala
@@ -5,5 +5,29 @@ case class Grid(rows: Vector[Vector[Cell]]) {
   def this() = this(Vector.tabulate(6, 7) { (row, col) => Cell(None) })
   def cell(row: Int, col: Int): Cell = rows(row)(col)
   def replaceCell(row: Int, col: Int, cell: Cell): Grid = copy(rows.updated(row, rows(row).updated(col, cell)))
+
+  def drop(column: Int, piece: Piece): Grid = {
+    val idx = this.rows.indexWhere(row => !row(column).isSet)
+    if(idx > -1) {
+      this.replaceCell(idx, column, Cell(Some(piece)))
+    } else {
+      this
+    }
+  }
+
+  def reset(): Grid = {
+    new Grid
+  }
+
+  override def toString: String = {
+    val builder = new StringBuilder
+    for (row <- this.rows.reverse) {
+      for (col <- row) {
+        builder.append(col)
+      }
+      builder.append("\n")
+    }
+    builder.toString()
+  }
 }
 
