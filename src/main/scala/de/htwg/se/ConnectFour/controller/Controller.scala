8e35792fb397f6c716dba75e4718d7a8236527e7
victor <victor.g1@web.de>
2021-04-28 22:59:44 +0200

    controller update

--- src/main/scala/de/htwg/se/ConnectFour/controller/Controller.scala
+++ src/main/scala/de/htwg/se/ConnectFour/controller/Controller.scala
@@ -1,12 +1,27 @@
 package de.htwg.se.ConnectFour.controller
 
 import de.htwg.se.ConnectFour._
+import de.htwg.se.ConnectFour.model.{Grid, Piece}
+import de.htwg.se.ConnectFour.util.Observable
 
-class Controller {
+class Controller(var grid:Grid) extends Observable {
   def createGrid(): Unit = {
-    //grid = new Grid()
+    reset()
+    notifyObservers
   }
 
-}
+  def reset(): Unit = {
+    grid = new Grid
+    notifyObservers
+  }
 
+  def dropPiece(col: Int,piece: Piece): Unit = {
+    grid = grid.drop(col,piece)
+    notifyObservers
+  }
+
+  def gridPrint(): String = grid.toString
+
+
+}
 
