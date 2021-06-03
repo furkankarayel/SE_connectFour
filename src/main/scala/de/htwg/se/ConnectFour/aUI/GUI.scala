bdd5833a27312cdf2d3acdd567bd8fd1449ed5d0
victor <victor.g1@web.de>
2021-06-03 15:56:31 +0200

    gui update

--- src/main/scala/de/htwg/se/ConnectFour/aUI/GUI.scala
+++ src/main/scala/de/htwg/se/ConnectFour/aUI/GUI.scala
@@ -17,66 +17,101 @@ import scalafx.scene.text.Text
 case class GUI(controller:Controller) extends UI with Observer with JFXApp {
 
   var gameState:GameState = GameState(controller)
-
+  controller.add(this)
   def run(): Unit = {
     main(Array())
   }
-  stage = new PrimaryStage {
-    title.value = "ConnectFour Game"
+
+  def typeName():Unit = {
     val dialog = new TextInputDialog(defaultValue = "Detlef") {
-      initOwner(stage)
-      initOwner(stage)
-      title = "ConnectFour Game"
-      headerText = "Welcome to Connect Four!"
-      contentText = "Player One please enter your name:"
-    }
+    initOwner(stage)
+    title = "ConnectFour Game"
+    headerText = "Welcome to Connect Four!"
+      var number = ""
+      if(controller.players.size == 0) number = "one" else number = "two"
+    contentText = "Player " + number + " please enter your name:"
+  }
 
     val result = dialog.showAndWait()
 
     result match {
-      case Some(name) => println("Your name: " + name)
-      case None       => println("Please type a name!")
+    case Some(name) => controller.addPlayer(name)
+    case None       => println("Please type a name!")
+  }
+  }
+
+  def begin():Unit = {
+    do {
+      val playerSize = controller.players.size
+      playerSize match {
+        case 0 => typeName()
+        case 1 => typeName()
+      }
+    } while (controller.players.size<2)
+    gameState.handle("")
+  }
+
+  def gameGrid: GridPane = new GridPane {
+    style = "-fx-border-color: #353535; -fx-background-color #f0e68c -fx-border-width: 0 2 0 0"
+    for (_ <- 0 until controller.colCount) {
+      val row = new RowConstraints() {
+        percentHeight = 100.0 / controller.rowCount
+      }
+      rowConstraints.add(row)
+      val column = new ColumnConstraints() {
+        percentWidth = 100.0 / controller.colCount
+      }
+      styleClass = List("gameGridRowColumn")
+      columnConstraints.add(column)
     }
+  }
+
+  def logo:HBox = new HBox {
+    style = "-fx-border-color: #353535; -fx-border-width: 0 2 0 0"
+    padding = Insets(5, 10, 5, 10)
+    children = Seq(
+      new Text {
+        text = "Connect"
+        style = "-fx-font: normal bold 50pt sans-serif"
+        fill = new LinearGradient(
+          endX = 0,
+          stops = Stops(Red, DarkRed))
+        effect = new DropShadow {
+          color = Black
+          radius = 5
+          spread = 0.3
+        }
+      },
+      new Text {
+        text = "Four"
+        style = "-fx-font: normal bold 50pt sans-serif"
+        fill = new LinearGradient(
+          endX = 0,
+          stops = Stops(Yellow, LightYellow)
+        )
+        effect = new DropShadow {
+          color = Black
+          radius = 5
+          spread = 0.3
+        }
+      },
+    )
+  }
+
+  stage = new PrimaryStage {
+    title.value = "ConnectFour Game"
+    minWidth = 650
+    minHeight = 800
 
     scene = new Scene {
-      fill = Color.rgb(120, 120, 68)
-      content = new HBox {
-        padding = Insets(5, 10, 5, 10)
-        children = Seq(
-          new Text {
-            text = "Connect"
-            style = "-fx-font: normal bold 50pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Red, DarkRed))
-              effect = new DropShadow {
-              color = Black
-              radius = 5
-              spread = 0.3
-            }
-          },
-          new Text {
-            text = "Four"
-            style = "-fx-font: normal bold 50pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Yellow, LightYellow)
-            )
-            effect = new DropShadow {
-              color = Black
-              radius = 5
-              spread = 0.3
-            }
-          },
-          new Rectangle() {
-            x = 80
-            y = 40
-            width = 100
-            height = 100
-          }
-        )
+      root = new BorderPane {
+        style = "-fx-border-color: #353535; -fx-background-color #f0e68c -fx-border-width: 0 2 0 0"
+        top = logo
+        center = gameGrid
       }
+      begin()
     }
+
   }
 
   override def processInput(input: String):Unit = {
--- src/main/scala/de/htwg/se/ConnectFour/aUI/states/GUI/InitialState.scala
+++ src/main/scala/de/htwg/se/ConnectFour/aUI/states/GUI/InitialState.scala
@@ -2,19 +2,11 @@ package de.htwg.se.ConnectFour.aUI.states.GUI
 
 import de.htwg.se.ConnectFour.controller.Controller
 import de.htwg.se.ConnectFour.util.State
-import jdk.javadoc.internal.doclets.toolkit.util.DocFinder.Input
 
 case class InitialState(controller: Controller) extends State[GameState] {
   override def handle(input: String,state: GameState): Unit = {
     if (controller.players.size == controller.maxPlayers) {
-      controller.currentPlayer = controller.players(0)
-
-        //controller.drop(Some(input))
-        state.changeState(DropState(controller))
-
-    }
-    else if (controller.players.size < 3) {
-      //controller.addPlayer()
+      state.changeState(DropState(controller))
     }
   }
 }
