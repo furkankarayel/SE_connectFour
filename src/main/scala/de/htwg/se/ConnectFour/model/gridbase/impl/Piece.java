272925bbc9e73b11de747834a38da6867ccbf5a7
furkankarayel <furkarayel@hotmail.com>
2021-06-17 23:35:49 +0200

    Restructured

---
+++ src/main/scala/de/htwg/se/ConnectFour/model/gridbase/impl/Piece.java
@@ -0,0 +1,14 @@
+case
+
+class Piece(player:Player) {
+
+    override def
+    toString:String =
+
+    {
+        player.playerNumber match {
+        case 1 =>Console.RED + "☻ "
+        case 2 =>Console.YELLOW + "☻ "
+    }
+    }
+}
