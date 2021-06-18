0da0e8b0e0401077eda51b5e1ade95021ed63b6c
victor <victor.g1@web.de>
2021-06-18 10:04:08 +0200

    worked on DI

--- src/main/scala/de/htwg/se/ConnectFour/Game.scala
+++ src/main/scala/de/htwg/se/ConnectFour/Game.scala
@@ -1,19 +1,21 @@
 package de.htwg.se.ConnectFour
 
+import com.google.inject.{Guice, Injector}
 import de.htwg.se.ConnectFour.aUI.UIFactory
 import de.htwg.se.ConnectFour.controller._
 import de.htwg.se.ConnectFour.controller.impl.ControllerImpl
 import de.htwg.se.ConnectFour.model.grid.impl.GridImpl
 
+import java.lang.ModuleLayer.Controller
 import scala.util.{Failure, Success, Try}
 
 object Game {
   def main(args: Array[String]): Unit = {
-    val grid = new GridImpl()
-    val controller = new ControllerImpl(grid)
+    val injector: Injector = Guice.createInjector(new GameModule())
+    injector.getInstance(classOf[Controller])
     val uiType = "GUI"
 
-    Try(UIFactory(uiType,controller)) match {
+    Try(UIFactory(uiType,Controller: ControllerImpl)) match {
       case Success(v) => println("Bye.")
       case Failure(v) => println("Could not create UI" + v.getMessage + v.getCause)
     }
--- src/main/scala/de/htwg/se/ConnectFour/GameModule.scala
+++ src/main/scala/de/htwg/se/ConnectFour/GameModule.scala
@@ -1,9 +1,15 @@
 package de.htwg.se.ConnectFour
 import com.google.inject.AbstractModule
+import de.htwg.se.ConnectFour.controller.impl.ControllerImpl
+import de.htwg.se.ConnectFour.model.grid.Grid
+import de.htwg.se.ConnectFour.model.grid.impl.GridImpl
 import net.codingwell.scalaguice.ScalaModule
-import com.google.inject.name.Names
-class GameModule {
-  override def configure(): Unit = {
+import java.lang.ModuleLayer.Controller
 
+class GameModule extends AbstractModule with ScalaModule {
+  override def configure(): Unit = {
+      bind[Grid].to[GridImpl]
+      bind[Grid].toInstance(new GridImpl())
+      bind[Controller].to[ControllerImpl]
   }
 }
